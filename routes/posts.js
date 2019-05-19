bce8357ef8be0adb555fb1c82322630d260b105d
Rohan Faiyaz Khan <rohankhan170@gmail.com>
2019-05-19 21:25:58 +0600

    Added all backend routes

--- routes/posts.js
+++ routes/posts.js
@@ -113,14 +113,13 @@ router.put('/like/:id', auth, async (req, res) => {
 	try {
 		const post = await Post.findById(req.params.id)
 
-		if(!post){
-			return res.status(404).send({msg: "Post not found"})
+		if (!post) {
+			return res.status(404).send({ msg: 'Post not found' })
 		}
 
 		if (post.likes.filter(like => like.user.toString() === req.user.id).length > 0) {
 			return res.status(400).json({ msg: 'User already liked this post' })
 		}
-
 
 		post.likes.unshift({ user: req.user.id })
 
@@ -145,16 +144,16 @@ router.put('/unlike/:id', auth, async (req, res) => {
 	try {
 		const post = await Post.findById(req.params.id)
 
-		if(!post){
-			return res.status(404).send({msg: "Post not found"})
+		if (!post) {
+			return res.status(404).send({ msg: 'Post not found' })
 		}
 
 		if (post.likes.filter(like => like.user.toString() === req.user.id).length === 0) {
 			return res.status(400).json({ msg: 'User has not liked this post' })
 		}
 
-		const removeIndex = post.likes.map(like => like.user.toString()).indexOf(req.user.id)
-		post.likes.pull(post.likes[removeIndex]._id)
+		const removeLike = post.likes.find(like => like.user.toString())
+		post.likes.pull(removeLike._id)
 
 		await post.save()
 
@@ -163,6 +162,71 @@ router.put('/unlike/:id', auth, async (req, res) => {
 		console.error(e.message)
 		if (e.kind === 'ObjectId') {
 			return res.status(404).send({ msg: 'Post not found' })
+		}
+		res.status(500).send('Server error')
+	}
+})
+
+/* 
+*  @route   POST api/posts/comment/:id
+*  @desc    Comment on a post 
+*  @access  Private 
+*/
+router.post('/comment/:id', [auth, [check('text', 'Text is required').exists()]], async (req, res) => {
+	const errors = validationResult(req)
+	if (!errors.isEmpty()) {
+		return res.status(400).json({ errors: errors.array() })
+	}
+
+	try {
+		const user = await User.findById(req.user.id).select('-password')
+		const post = await Post.findById(req.params.id)
+
+		const newComment = {
+			text: req.body.text,
+			name: user.name,
+			avatar: user.avatar,
+			user: user.id
+		}
+
+		post.comments.unshift(newComment)
+		await post.save()
+		res.json(post.comments)
+	} catch (e) {
+		console.error(e.message)
+		if (e.kind === 'ObjectId') {
+			return res.status(404).send({ msg: 'Post not found' })
+		}
+		res.status(500).send('Server error')
+	}
+})
+
+/* 
+*  @route   DELETE api/posts/comment/:id
+*  @desc    Delte comment on a post 
+*  @access  Private 
+*/
+router.delete('/comment/:id/:comment_id', auth, async (req, res) => {
+	try {
+		const post = await Post.findById(req.params.id)
+
+		const comment = post.comments.find(comment => comment._id.toString() === req.params.comment_id)
+
+		if (!comment) {
+			return res.status(404).send({ msg: 'Comment not found' })
+		}
+
+		if (comment.user.toString() !== req.user.id) {
+			return res.status(401).json({ msg: 'Unable to authenticate' })
+		}
+
+		post.comments.pull(comment._id)
+		await post.save()
+		res.json(post.comments)
+	} catch (e) {
+		console.error(e.message)
+		if (e.kind === 'ObjectId') {
+			return res.status(404).send({ msg: 'Comment not found' })
 		}
 		res.status(500).send('Server error')
 	}
