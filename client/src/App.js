ed9cae7f1e63cd99c07dc724042e2c533a4e1d40
Rohan Faiyaz Khan <rohankhan170@gmail.com>
2019-05-22 11:23:55 +0600

    Setup redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React, { Fragment } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import store from './store'
 
 import './App.css'
 import Landing from './components/Landing'
@@ -8,18 +10,20 @@ import Login from './components/auth/Login'
 import Register from './components/auth/Register'
 
 const App = () => (
-	<Router>
-		<Fragment>
-			<Navbar />
-			<Route exact path="/" component={Landing} />
-			<section className="container">
-				<Switch>
-					<Route exact path="/register" component={Register} />
-					<Route exact path="/login" component={Login} />
-				</Switch>
-			</section>
-		</Fragment>
-	</Router>
+	<Provider store={store}>
+		<Router>
+			<Fragment>
+				<Navbar />
+				<Route exact path="/" component={Landing} />
+				<section className="container">
+					<Switch>
+						<Route exact path="/register" component={Register} />
+						<Route exact path="/login" component={Login} />
+					</Switch>
+				</section>
+			</Fragment>
+		</Router>
+	</Provider>
 )
 
 export default App
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,5 +1,5 @@
 import React, { Fragment, useState } from 'react'
-import axios from 'axios' 
+import { Link } from 'react-router-dom'
 
 const Register = () => {
 	const [formData, setFormData] = useState({
@@ -11,57 +11,73 @@ const Register = () => {
 
 	const { name, email, password, password2 } = formData
 
-  const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value })
-  
-  const onSubmit = async (e) => {
-    e.preventDefault()
-    if(password !== password2){
-      console.log("Passwords do not match");
-    }else{
-      const newUser = {
-				name,
-				email,
-				password
-			}
+	const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value })
 
-			try {
-				
-				const config = {
-					headers: {
-						'Content-Type':'application/json'
-					}
-				}
-
-				const body = JSON.stringify(newUser)
-				const res = await axios.post('/api/users', body, config)
-				console.log(res.data)
-
-			}catch(e){
-				console.error(e.response.data)
-			}
-    }
-  }
+	const onSubmit = async e => {
+		e.preventDefault()
+		if (password !== password2) {
+			console.log('Passwords do not match')
+		} else {
+			console.log('Success')
+		}
+	}
 
 	return (
 		<Fragment>
+			<h1 class="large text-primary">Sign Up</h1>
+			<p class="lead">
+				<i class="fas fa-user" /> Create Your Account
+			</p>
 			<form className="form" onSubmit={e => onSubmit(e)}>
 				<div className="form-group">
-					<input type="text" placeholder="Name" name="name" value={name} onChange={e => onChange(e)} required />
+					<input
+						type="text"
+						placeholder="Name"
+						name="name"
+						value={name}
+						onChange={e => onChange(e)}
+						required
+					/>
 				</div>
 				<div className="form-group">
-					<input type="email" placeholder="Email Address" value={email} onChange={e => onChange(e)} name="email" />
+					<input
+						type="email"
+						placeholder="Email Address"
+						value={email}
+						onChange={e => onChange(e)}
+						name="email"
+					/>
 					<small className="form-text">
 						This site uses Gravatar so if you want a profile image, use a Gravatar email
 					</small>
 				</div>
 				<div className="form-group">
-					<input type="password" placeholder="Password" name="password" value={password} onChange={e => onChange(e)} required minLength="6" />
+					<input
+						type="password"
+						placeholder="Password"
+						name="password"
+						value={password}
+						onChange={e => onChange(e)}
+						required
+						minLength="6"
+					/>
 				</div>
 				<div className="form-group">
-					<input type="password" placeholder="Confirm Password" name="password2" value={password2} onChange={e => onChange(e)} required minLength="6" />
+					<input
+						type="password"
+						placeholder="Confirm Password"
+						name="password2"
+						value={password2}
+						onChange={e => onChange(e)}
+						required
+						minLength="6"
+					/>
 				</div>
 				<input type="submit" className="btn btn-primary" value="Register" />
 			</form>
+			<p class="my-1">
+        Already have an account? <Link to="/register">Sign In</Link>
+      </p>
 		</Fragment>
 	)
 }
