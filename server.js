696595460ef070f883787487dbbc41b67f8ed507
Rohan Faiyaz Khan <rohankhan170@gmail.com>
2019-06-04 01:21:11 +0600

    Config for Heroku

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 
 const connectDB = require('./config/db')
 
@@ -15,6 +16,14 @@ app.use('/api/users', require('./routes/users'))
 app.use('/api/auth', require('./routes/auth')) 
 app.use('/api/profile', require('./routes/profile')) 
 app.use('/api/posts', require('./routes/posts')) 
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express,static('client/build'))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
