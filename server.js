47fd8e5f6e27fe187d8a008afaeedb88a8945dcc
Rohan Faiyaz Khan <rohankhan170@gmail.com>
2019-06-04 01:31:06 +0600

    Fixed typo in config

--- server.js
+++ server.js
@@ -12,19 +12,19 @@ connectDB()
 // Init middleware
 app.use(express.json({ extended: false }))
 
-app.use('/api/users', require('./routes/users')) 
-app.use('/api/auth', require('./routes/auth')) 
-app.use('/api/profile', require('./routes/profile')) 
-app.use('/api/posts', require('./routes/posts')) 
+app.use('/api/users', require('./routes/users'))
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/profile', require('./routes/profile'))
+app.use('/api/posts', require('./routes/posts'))
 
-if(process.env.NODE_ENV === 'production'){
-    app.use(express,static('client/build'))
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'))
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    })
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+	})
 }
 
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`)
+	console.log(`Listening on port ${port}`)
 })
